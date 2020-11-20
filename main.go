77a74a77244ac471bc5746ff0d2d8a0942737594
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-20 14:42:22 +0800

    :hammer:

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/gofiber/fiber/v2"
-
-	"go-with-redis/controller/GetUserHandler.go"
+	"github.com/gofiber/fiber"
+	"github.com/vindecodex/go-with-redis/controller"
 )
 
 func main() {
-	fmt.Println("MAIN")
-
 	app := fiber.New()
 
-	const url = "https://api.github.com/users/"
-
-	app.Get("/api/:search", controller.GetUserHandler())
-
-	fmt.Println("Listening on port: 1234")
-	log.Fatal(app.Listen(":1234"))
+	app.Get("/", controller.GetUserHandler)
+	app.Listen(":4321")
 }
