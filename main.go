439bbb38906e46eff33269909500b2c078fb371a
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-23 16:12:30 +0800

    unmarshal

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
-	"github.com/vindecodex/go-with-redis/controller"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-
-	app.Get("/", controller.GetUserHandler)
+	url := "https://api.github.com/users/"
+	var data = nil
+	app.Get("/:user", func(c *fiber.Ctx) error {
+		resp, err := http.Get(url + c.Params("user"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		json.Unmarshal([]byte(resp), &data)
+	})
 	app.Listen(":4321")
 }
