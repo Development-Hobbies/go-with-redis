25a5c66a2a82887d536267d37e2d3165d1c75c68
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-23 16:24:29 +0800

    :tada:

--- main.go
+++ main.go
@@ -11,16 +11,21 @@ import (
 func main() {
 	app := fiber.New()
 	url := "https://api.github.com/users/"
+
 	app.Get("/:user", func(c *fiber.Ctx) error {
+
 		resp, err := http.Get(url + c.Params("user"))
 		if err != nil {
 			log.Fatalln(err)
 		}
+
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatalln(err)
 		}
+
 		return c.Send(body)
 	})
+
 	app.Listen(":4321")
 }
