85fc9acd7efcc2caaa1bed88d827e887e46312f6
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-24 16:37:41 +0800

    :rocket:

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,6 +13,7 @@ import (
 func main() {
 	app := fiber.New()
 	url := "https://api.github.com/users/"
+	var ctx = context.Background()
 
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
@@ -21,17 +23,32 @@ func main() {
 
 	app.Get("/:user", func(c *fiber.Ctx) error {
 
-		resp, err := http.Get(url + c.Params("user"))
+		user := c.Params("user")
+
+		val, err := rdb.Get(ctx, user).Result()
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalln(err)
+		if len(val) > 0 {
+			resp, err := http.Get(url + user)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			err = rdb.Set(ctx, "user", body, 0).Err()
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			return c.Send(body)
 		}
 
-		return c.Send(body)
+		return c.SendString(val)
 	})
 
 	app.Listen(":4321")
