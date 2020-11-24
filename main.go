ee020cc5703d5353cb8f96caeb54856d015c1a63
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-24 16:41:40 +0800

    :rocket:

--- main.go
+++ main.go
@@ -25,24 +25,24 @@ func main() {
 
 		user := c.Params("user")
 
-		val, err := rdb.Get(ctx, user).Result()
+		val, err := rdb.Get(ctx, "vindecodex").Result()
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalln("Error 30:")
 		}
 
 		if len(val) > 0 {
 			resp, err := http.Get(url + user)
 			if err != nil {
-				log.Fatalln(err)
+				log.Fatalln("Error 36:")
 			}
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
-				log.Fatalln(err)
+				log.Fatalln("Error 40:")
 			}
 
 			err = rdb.Set(ctx, "user", body, 0).Err()
 			if err != nil {
-				log.Fatalln(err)
+				log.Fatalln("Error 45:")
 			}
 
 			return c.Send(body)
